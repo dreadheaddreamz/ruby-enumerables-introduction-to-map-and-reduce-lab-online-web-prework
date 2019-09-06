4256f1ec2ec7272b2e560702be430bd77e363a63
James Batsell <jamesbatsell05@gmail.com>
2019-09-06 07:19:13 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -8,15 +8,15 @@ end
 return new_array
 end
 
-def map_to_no_change(source_array)
-  counter = 0
-  new_array = []
-  while counter < source_array.length do
-    new_array << source_array[counter]
-    counter += 1
+ def map_to_no_change (source_array)
+   counter = 0 
+   new_array = []
+   while counter < source_array.length do
+     new_array << source_array[counter]
+     counter += 1
   end
   new_array
-end
+ end
     
   end
 
